4b8af9c1563a2d4ef63abdfa8dd62c75190b280d
Sontome <93179211+Sontome@users.noreply.github.com>
2025-04-19 22:45:56 +0700

    Add files via upload

--- reloadcookievna.py
+++ reloadcookievna.py
@@ -4,5 +4,5 @@ import subprocess
 while True:
     print("🕒 Chạy getcokivna.py để renew cookie...")
     subprocess.run(["python", "getcokivna.py"])
-    print("✅ Xong! Ngủ 5 phút...\n")
-    time.sleep(300)  # 300 giây = 5 phút
+    print("✅ Xong! Ngủ 180s ..\n")
+    time.sleep(180)  # 300 giây = 5 phút
