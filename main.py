f768cf30b2456b0ace8aff1bea522b8e00a44dc5
Sontome <93179211+Sontome@users.noreply.github.com>
2025-08-24 17:15:25 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, File, UploadFile, Form, BackgroundTasks
 from fastapi.middleware.cors import CORSMiddleware
+from backend_read_PDF_VNA_VN import reformat_VNA_VN
+
+from fastapi.responses import FileResponse
 from backend_api_vj import api_vj
 from backend_api_vj_v2 import api_vj_v2
 from backend_api_vj_lowest_v2 import lay_danh_sach_ve_re_nhat
@@ -592,3 +595,32 @@ async def vna_checkpnr(pnr,ssid):
     result = await checkPNR(pnr,ssid)
     print(result)
     return result
+@app.post("/process-pdf-vna-vn/")
+async def process_pdf_VNA_VN(
+    background_tasks: BackgroundTasks,
+    file: UploadFile = File(...),
+    option: str = Form("") # tham số test
+           # ví dụ chỉ xử lý 1 page
+):
+    temp_path = f"VN{file.filename}"
+    
+    with open(temp_path, "wb") as f:
+        f.write(await file.read())
+
+    # Tạo đường dẫn file output
+    
+
+    # Gọi hàm xử lý PDF, truyền thêm param nếu cần
+    reformat_VNA_VN(temp_path, new_text=option)
+    
+
+    # Xoá file input nếu không cần giữ
+    if os.path.exists(temp_path):
+        os.remove(temp_path)
+    background_tasks.add_task(os.remove, "output"+temp_path)
+    # Trả file PDF đã xử lý về cho client
+    return FileResponse(
+        path="output"+temp_path,
+        filename=f"{file.filename}",
+        media_type="application/pdf"
+    )
