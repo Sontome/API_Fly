2eec85490943d81fd05c8ab6011a1c25a1d3fa29
Sontome <93179211+Sontome@users.noreply.github.com>
2025-08-26 16:24:17 +0700

    Update backend_read_PDF_VJ.py

--- backend_read_PDF_VJ.py
+++ backend_read_PDF_VJ.py
@@ -70,7 +70,7 @@ def replace_text_between_phrases(pdf_path, output_path,
     if found_time and found_date:
         try:
             flight_dt = datetime.strptime(f"{found_date} {found_time}", "%d/%m/%Y %H:%M")
-            checkin_dt = flight_dt - timedelta(hours=4)
+            checkin_dt = flight_dt - timedelta(hours=3)
             periodt = "(Sang)" if checkin_dt.hour < 12 else "(Chieu)"
             note_str = f"Luu y: Quy khach vui long den san bay truoc {checkin_dt.strftime('%d/%m/%Y %H:%M')} {periodt} de lam thu tuc\n len may bay."
             print(f"[DEBUG] Giờ check-in: {note_str}")
@@ -177,4 +177,5 @@ def reformat_VJ(input_pdf,output_path,new_text=NEW_TEXT):
 # ===== TEST =====
 
 #reformat_VJ("input.pdf","output.pdf")
+
 #extract_first_page("output.pdf")
