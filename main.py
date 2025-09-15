729c4382815a82f203369dda9255783ea6c1527d
Sontome <93179211+Sontome@users.noreply.github.com>
2025-09-15 22:51:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -779,7 +779,7 @@ async def checkdate_VNA_Api(
 import httpx
 from fastapi.responses import JSONResponse
 
-GAS_URL = "https://script.google.com/macros/s/AKfycbyLhg7YsLDfgTNJdvYwLrEQyJ6eW4gg5QagYoNNKcwbbkk4winkrhTYJmQ-K4_LUDFe2g/exec"
+GAS_URL = "https://script.google.com/macros/s/AKfycbwjVOSXk7K6NxpwF7AV4gh30SBaaMvBOT5d4Kbz7nWBZBxyHa2RW_1LEMgeW9s8Hp2wgg/exec"
 
 @app.options("/proxy-gas")
 async def proxy_gas_options():
@@ -1004,5 +1004,6 @@ def sendmailvj(
     except Exception as e:
 
         return (str(e))   
+
 
 
