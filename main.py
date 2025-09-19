be93ad4b230f20f3841e045b6ea7525cb10ade7d
Sontome <93179211+Sontome@users.noreply.github.com>
2025-09-19 23:35:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -780,7 +780,7 @@ async def checkdate_VNA_Api(
 import httpx
 from fastapi.responses import JSONResponse
 
-GAS_URL = "https://script.google.com/macros/s/AKfycbz-nD85RpuA4GVQZYGDbH510ptISrBtQHszBu30kPu2rQLAJT6aB1DriEU4OozvxvT3qg/exec"
+GAS_URL = "https://script.google.com/macros/s/AKfycbw9VSCTZUjO3qBkz5He2xv0_BHkX5W1UNl6-5s1CWl2IsQ-d1k7IOiJwyvtzmpYh-mxjA/exec"
 
 @app.options("/proxy-gas")
 async def proxy_gas_options():
@@ -1043,6 +1043,7 @@ async def checkpaymentVNA(
 
     # Trả file output về cho client
     return res
+
 
 
 
