72d8b4132f31f7306997ce97bdb432d196f720b1
Sontome <93179211+Sontome@users.noreply.github.com>
2025-11-02 17:20:17 +0700

    Update backend_read_PDF_VNA_KR.py

--- backend_read_PDF_VNA_KR.py
+++ backend_read_PDF_VNA_KR.py
@@ -124,8 +124,8 @@ def replace_text_between_phrases(pdf_path,output_path,
                         ddmm = pre_night.strftime("%d/%m")
 
                         page.insert_text(
-                            (rect.x0-20, rect.y0 + 30),
-                            f"(Ra sân bay đêm ngày {ddmm})",
+                            (rect.x0-5, rect.y0 + 30),
+                            f"(Ra sân bay\n đêm {ddmm})",
                             fontsize=fs,
                             fontfile=FONT_ARIAL,
                             fontname="arial",
@@ -316,6 +316,7 @@ def reformat_VNA_KR(input_pdf,output_path,new_text=NEW_TEXT):
 
 
 #extract_first_page("output.pdf")
+
 
 
 
