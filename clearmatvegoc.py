ec09bd7b92f73aec2f2781ab523fc49a0bd7ecb2
Sontome <93179211+Sontome@users.noreply.github.com>
2026-02-02 16:17:27 +0700

    Create clearmatvegoc.py

---
+++ clearmatvegoc.py
@@ -0,0 +1,10 @@
+import os, time
+
+FOLDER = "/root/matvegoc"
+now = time.time()
+
+for f in os.listdir(FOLDER):
+    path = os.path.join(FOLDER, f)
+    if os.path.isfile(path):
+        if now - os.path.getmtime(path) > 400:
+            os.remove(path)
