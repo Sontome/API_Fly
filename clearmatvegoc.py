8baee415771b59d40ded9db7a0033bb2a16aae16
Sontome <93179211+Sontome@users.noreply.github.com>
2026-02-05 13:50:33 +0700

    Update clearmatvegoc.py

--- clearmatvegoc.py
+++ clearmatvegoc.py
@@ -7,5 +7,5 @@ now = time.time()
 for f in os.listdir(FOLDER):
     path = os.path.join(FOLDER, f)
     if os.path.isfile(path):
-        if now - os.path.getmtime(path) > 156400:
+        if now - os.path.getmtime(path) > 23328000:
             os.remove(path)
