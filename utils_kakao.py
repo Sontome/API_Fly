93ce560ebec8df7d12e1cff9b92dadcfc2362684
Sontome <93179211+Sontome@users.noreply.github.com>
2026-03-03 20:25:31 +0700

    Update utils_kakao.py

--- utils_kakao.py
+++ utils_kakao.py
@@ -2,18 +2,18 @@ from datetime import datetime
 from backend_supabase_kakao import update_sent_phone,get_unsent_latest_kakao
 from getinfopnr_vj import checkpnr_vj
 from backendapi1a import checkmatvechoVNA
+import asyncio
 
-def process_send_kakao(PNR, type, phone):
+async def process_send_kakao(PNR, type, phone):
     current_time = datetime.now().strftime("%Hh%Mp ngày %d/%m/%Y")
 
-    # Xác định nội dung theo type
     if type == "ISSUED":
         prefix = f"PNR {PNR} đã xuất vé thành công vào {current_time}"
-    else:  # mặc định HOLD
+    else:
         prefix = f"PNR {PNR} đã giữ chỗ thành công vào {current_time}"
 
-    # Check VJ trước
-    result_vj = checkpnr_vj(PNR)
+    # await nè đại ca
+    result_vj = await checkpnr_vj(PNR)
 
     if result_vj and "kakaomess" in result_vj:
         kakaomess = result_vj["kakaomess"]
@@ -27,8 +27,7 @@ def process_send_kakao(PNR, type, phone):
         update_sent_phone(phone)
         return
 
-    # Nếu không có thì check VNA
-    result_vna = checkmatvechoVNA(PNR, "checkvecho")
+    result_vna = await checkmatvechoVNA(PNR, "checkvecho")
 
     if result_vna and "kakaomess" in result_vna:
         kakaomess = result_vna["kakaomess"]
@@ -41,5 +40,5 @@ def process_send_kakao(PNR, type, phone):
         )
         update_sent_phone(phone)
 if __name__ == "__main__":
-    process_send_kakao("D422P2", "ISSUED", "0764301092")
-    process_send_kakao("G49B3G", "HOLD", "0764301092")
+    asyncio.run(process_send_kakao("D422P2", "ISSUED", "0764301092"))
+    asyncio.run(process_send_kakao("G49B3G", "HOLD", "0764301092"))
