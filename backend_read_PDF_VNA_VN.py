5e5410a0c07d04bd40ba21c51cc47bd29a9c044c
Sontome <93179211+Sontome@users.noreply.github.com>
2026-03-12 21:27:52 +0700

    Update backend_read_PDF_VNA_VN.py

--- backend_read_PDF_VNA_VN.py
+++ backend_read_PDF_VNA_VN.py
@@ -260,6 +260,8 @@ def replace_text_between_phrases(pdf_path,output_path,
                 (x, y),
                 "Hành lý: ",
                 fontsize=fs*1.2,
+                fontfile=FONT_ARIAL_BOLD,
+                fontname="arial",
                 
                 fill=(0.0118, 0.1647, 0.2588)
             )
@@ -540,6 +542,7 @@ def reformat_VNA_VN(input_pdf,output_path,new_text=NEW_TEXT,type=0):
 
 
 #reformat_VNA_VN("pdf1.pdf","output.pdf",type=0)
+
 
 
 
