fdb06a1ee01f5995e798ff506e63097604ca90ae
Sontome <93179211+Sontome@users.noreply.github.com>
2026-03-12 20:46:44 +0700

    Update backend_read_PDF_VNA_VN.py

--- backend_read_PDF_VNA_VN.py
+++ backend_read_PDF_VNA_VN.py
@@ -246,7 +246,7 @@ def replace_text_between_phrases(pdf_path,output_path,
                 addon = ""
     
             # xóa dòng cũ
-            rect_del = fitz.Rect(rect.x0, rect.y0, page.rect.x1, rect.y1)
+            rect_del = rect
             page.add_redact_annot(rect_del)
             page.apply_redactions()
     
@@ -538,6 +538,7 @@ def reformat_VNA_VN(input_pdf,output_path,new_text=NEW_TEXT,type=0):
 
 
 #reformat_VNA_VN("pdf1.pdf","output.pdf",type=0)
+
 
 
 
