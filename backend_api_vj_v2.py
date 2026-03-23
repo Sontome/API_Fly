961824f442e56c806c3dff5166b4f1ea118be810
Sontome <93179211+Sontome@users.noreply.github.com>
2026-03-23 10:15:39 +0700

    Update backend_api_vj_v2.py

--- backend_api_vj_v2.py
+++ backend_api_vj_v2.py
@@ -478,8 +478,8 @@ def extract_flight(data, list_key, giá_hành_lý,phi_chieu_di):
         stt = 1
         for chuyen in list_chuyen:
             # ❌ bỏ qua nếu số điểm dừng khác 1
-            # if chuyen.get("NumberOfStops") != 1:
-            #     continue
+            if chuyen.get("NumberOfStops") != 1:
+                continue
             if chuyen['fareOption']:
                 segments = chuyen.get("segmentOptions", [])
                 if not segments:
