35764f8d177f383055710927b4f130f8b3666a4f
Sontome <93179211+Sontome@users.noreply.github.com>
2026-05-19 17:07:14 +0700

    Update log_mail_queue_service.py

--- services_mail/log_mail_queue_service.py
+++ services_mail/log_mail_queue_service.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 from datetime import datetime, timezone
 
-from pnr_trip_info_service import (
+from .pnr_trip_info_service import (
     PnrTripInfoService
 )
 
