7c337d7db0a8004bcc1880e7836d2541591fe437
Sontome <93179211+Sontome@users.noreply.github.com>
2026-05-20 10:46:56 +0700

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -327,8 +327,8 @@ try:
     flight_change_rules = [
     
         {
-            # "sender": "noreply.19001886@ses.vietjetair.com",
-            "sender": "devilrauxanhk17@gmail.com",
+            "sender": "noreply.19001886@ses.vietjetair.com",
+            
             "subject": "Thông báo lịch bay thay đổi",
             "hang": "VJ"
         },
