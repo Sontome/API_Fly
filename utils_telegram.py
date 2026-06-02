bacb911042c4c8441b8b020dc062e220b0978db0
Sontome <93179211+Sontome@users.noreply.github.com>
2026-06-02 17:40:57 +0700

    Update utils_telegram.py

--- utils_telegram.py
+++ utils_telegram.py
@@ -2,6 +2,7 @@ import httpx
 import json
 import os
 import asyncio
+from dotenv import load_dotenv
 load_dotenv()
 # 👉 Thêm token và chat_id vào config, hoặc gán trực tiếp luôn
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "")
