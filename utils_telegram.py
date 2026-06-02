45d58fd91fd4521ae017807a6de045078262d762
Sontome <93179211+Sontome@users.noreply.github.com>
2026-06-02 17:37:28 +0700

    Update utils_telegram.py

--- utils_telegram.py
+++ utils_telegram.py
@@ -9,14 +9,15 @@ TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID", "-1002520783135")
 
 
 async def send_mess(message: str) -> bool:
-    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
-    payload = {
-        "chat_id": TELEGRAM_CHAT_ID,
-        "text": message,
-        "parse_mode": "HTML"  # Cho phép format đẹp
-    }
+    
 
     try:
+        url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+        payload = {
+            "chat_id": TELEGRAM_CHAT_ID,
+            "text": message,
+            "parse_mode": "HTML"  # Cho phép format đẹp
+        }
         async with httpx.AsyncClient() as client:
             res = await client.post(url, json=payload)
             if res.status_code != 200:
