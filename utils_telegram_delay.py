665e13b6d64f5421c238e44403f1cf61ea9455c8
Sontome <93179211+Sontome@users.noreply.github.com>
2026-06-02 17:07:37 +0700

    Update utils_telegram_delay.py

--- utils_telegram_delay.py
+++ utils_telegram_delay.py
@@ -3,7 +3,7 @@ import json
 import os
 
 # 👉 Thêm token và chat_id vào config, hoặc gán trực tiếp luôn
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "7843918695:AAGnIVDjJV52Citq0mn8zOQ0_Wr0MmD45qA")
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "")
 
 
 
