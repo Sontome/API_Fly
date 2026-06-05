416ea1bf9c6f28d4e09b8af9cb12cd74af6b9894
Sontome <93179211+Sontome@users.noreply.github.com>
2026-06-05 10:43:26 +0700

    Update holdbookingkeyVJ.py

--- holdbookingkeyVJ.py
+++ holdbookingkeyVJ.py
@@ -210,7 +210,7 @@ def get_payment_methods(token,bookingkey_departure, bookingkey_arrival=None):
                 f"{item.get('paymentdescription')}"
             )
         
-            if item.get("identifier") == "PL":
+            if item.get("identifier") == "PL" and item.get("paymentdescription") == "Pay Later":
                 paykey = item.get("key")
                 print("✅ Found Pay Later key")
                 break
