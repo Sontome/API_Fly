dd0f0830edb3a9fd55c9169a4ea169a132736d9c
Sontome <93179211+Sontome@users.noreply.github.com>
2026-07-30 11:31:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ from cachetools import TTLCache
 from check_price_stu_vna import check_price_stu_vna
 from routers.change_router import router as change_router
 from routers.spa_router import router as spa_router
-
+from backendapi1asun import repricePNR_SUN,beginRepricePNR_SUN
 
 load_dotenv()
 RATE_LIMIT_MINUTES = int(os.getenv("RATE_LIMIT_MINUTES", 3))
